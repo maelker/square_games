f83ead681fdce0708eb5dec349a6719de83c6bf5
Maël Kerloch <mael.kerloch@gmail.com>
2024-02-16 10:17:29 +0100

    Creation project

---
+++ src/main/java/com/cda25/springboot/square_games/SquareGamesApplication.java
@@ -0,0 +1,13 @@
+package com.cda25.springboot.square_games;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquareGamesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SquareGamesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cda25/springboot/square_games/SquareGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cda25.springboot.square_games;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquareGamesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
