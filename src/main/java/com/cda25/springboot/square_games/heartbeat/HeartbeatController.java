aa61aad5a828a599b4baba1b4b22738cbe8ed857
Maël Kerloch <mael.kerloch@gmail.com>
2024-02-16 11:15:23 +0100

    Create Controller HearbeatController.java

---
+++ src/main/java/com/cda25/springboot/square_games/heartbeat/HeartbeatController.java
@@ -0,0 +1,18 @@
+package com.cda25.springboot.square_games.heartbeat;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HeartbeatController {
+
+    @Autowired
+    private HeartbeatSensor heartBeatSensor;
+
+    @GetMapping("/heartbeat")
+    public int getHeartBeat() {
+        return heartBeatSensor.get();
+    }
+
+}
