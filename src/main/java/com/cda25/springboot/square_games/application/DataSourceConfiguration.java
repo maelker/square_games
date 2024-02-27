f4e8348998fdf921aaca4f9a3a526354af7f1a13
Maël Kerloch <mael.kerloch@gmail.com>
2024-02-27 12:27:37 +0100

    DataSource Configuration, you can now choose between h2 and mysql in application.properties

---
+++ src/main/java/com/cda25/springboot/square_games/application/DataSourceConfiguration.java
@@ -0,0 +1,38 @@
+package com.cda25.springboot.square_games.application;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfiguration {
+
+    @Bean
+    @ConfigurationProperties("application-h2.properties")
+    public DataSourceProperties dataSourceH2Properties() {
+        return new DataSourceProperties();
+    }
+    @Bean
+    @Profile("h2")
+    public DataSource dataSourceH2() {
+        return dataSourceH2Properties()
+                .initializeDataSourceBuilder()
+                .build();
+    }
+    @Bean
+    @ConfigurationProperties("application-mysql.properties")
+    public DataSourceProperties dataSourceMySQLProperties() {
+        return new DataSourceProperties();
+    }
+    @Bean
+    @Profile("mysql")
+    public DataSource dataSourceMySQL() {
+        return dataSourceMySQLProperties()
+                .initializeDataSourceBuilder()
+                .build();
+    }
+}
