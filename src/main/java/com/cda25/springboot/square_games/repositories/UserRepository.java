10230dcb2ee149e95ac8df30062d3175c41cd46d
Maël Kerloch <mael.kerloch@gmail.com>
2024-03-01 12:05:52 +0100

    added repository annotation

--- src/main/java/com/cda25/springboot/square_games/repositories/UserRepository.java
+++ src/main/java/com/cda25/springboot/square_games/repositories/UserRepository.java
@@ -3,9 +3,11 @@ package com.cda25.springboot.square_games.repositories;
 
 import com.cda25.springboot.square_games.entities_do.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     Optional<UserEntity> findByUsername(String username);
 }
