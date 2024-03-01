658158e072ab059c94664b02c2410b5244b2abe9
Maël Kerloch <mael.kerloch@gmail.com>
2024-03-01 11:32:31 +0100

    working on securityfilterchain

--- src/main/java/com/cda25/springboot/square_games/application/auth/SecurityConfig.java
+++ src/main/java/com/cda25/springboot/square_games/application/auth/SecurityConfig.java
@@ -1,26 +1,29 @@
 package com.cda25.springboot.square_games.application.auth;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Autowired
+    private MyUserDetailsService userDetailsService;
+
+    @Autowired
+    AuthenticationConfiguration authenticationConfiguration;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -28,60 +31,38 @@ public class SecurityConfig {
     }
 
     @Bean
-    public AuthenticationProvider authenticationProvider() {
-            DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-
-            authProvider.setUserDetailsService(userDetailsService());
-
-            authProvider.setPasswordEncoder(passwordEncoder());
-
-            return authProvider;
+    public AuthenticationManager authenticationManager() throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
-        return config.getAuthenticationManager();
-    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(AbstractHttpConfigurer::disable)
-                .exceptionHandling((exception)-> exception.authenticationEntryPoint(jwtAuthenticationEntryPoint).accessDeniedPage("/error/accedd-denied")
+        http.csrf(AbstractHttpConfigurer::disable).cors(Customizer.withDefaults());
+
+//      http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        http.sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http.userDetailsService(userDetailsService);
+
+        http.authenticationManager(authenticationConfiguration.getAuthenticationManager());
+
+        http.authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/login").permitAll()
+                .anyRequest().authenticated()
+        );
+
+
+
+        http.formLogin((form) -> form
+                        .loginPage("/login").permitAll()
                 )
-                .authorizeHttpRequests(request ->
-                        request
-                                .requestMatchers(
-                                        "/user/**"
-                                ).permitAll()
-                                //.requestMatchers("/api/v1/admin/resource").hasRole("ADMIN") replaced with annotation in AuthorizationController
-                                .anyRequest().authenticated()
-                )
-                .sessionManagement(manager -> manager.sessionCreationPolicy(STATELESS))
-                .authenticationProvider(authenticationProvider()).addFilterBefore(
-                        jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+                .logout(LogoutConfigurer::permitAll);
+
+        http.httpBasic(Customizer.withDefaults());
+
         return http.build();
-    }
-
-//    @Bean
-//    public UserDetailsService users() {
-//        UserDetails admin = User.builder()
-//                .username("admin")
-//                .password("password")
-//                .roles("ADMIN")
-//                .build();
-//        UserDetails user = User.builder()
-//                .username("user")
-//                .password("password")
-//                .roles("USER")
-//                .build();
-//
-//        return new InMemoryUserDetailsManager(admin, user);
-//    }
-
-    @Bean
-    public UserDetailsService userDetailsService(){
-        return username -> userDetailsService().loadUserByUsername(username);
     }
 
 }
